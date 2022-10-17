cb5fb1e17de6c06851964e63ae15d937fb04368e
JoshuaOldmixon <97045114+JoshuaOldmixon@users.noreply.github.com>
2022-10-17 15:33:54 -0400

    Update Main.py

--- S&P Ranking/Main.py
+++ S&P Ranking/Main.py
@@ -1,9 +1,24 @@
-from Features.Features import getData, rankPortfolio, getSpSymbols, buyOrder
+from Features import getData, rankPortfolio, getSpSymbols, trade, stop
+import datetime
+import schedule
+import time as tt
 
-symbols = getSpSymbols().table
-data = getData(symbols).table
-ranks = rankPortfolio(data).table
-symbols = ranks['symbol']
 
-for symbol in symbols[:50]:
-    buyOrder(symbol)
+def start():
+    symbols = getSpSymbols().table
+    data = getData(symbols).table
+    ranks = rankPortfolio(data).table
+    ranks = ranks[:50]
+    for symbol,score in zip(ranks['symbol'],ranks['scores']):            
+        trade(symbol,score)
+
+while True:
+    now = datetime.datetime.now()
+    time = (str(now.hour) + ':' + str(now.minute))
+    if time == '9:24':
+        start()
+    elif time == '15:55':
+        stop()
+    else:
+        print(str(time)+':'+str(now.second))
+        tt.sleep(1)
